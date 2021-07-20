e099cd9674f01dc75f01711924eddaa643a5139a
Alina Povazhnyuk <alina.l22@mail.ru>
2021-07-20 15:38:28 +0700

    format link fix

---
+++ blocks/format/format.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(function () {
+  $('.tabs-list__link').click(function (e) {
+    e.preventDefault();
+    var tabActive = $(this).attr('href'),
+        activeListClass = 'tabs-list__link--active',
+        ListItemClass = 'tabs-list__link',
+        tabWrap = '.tabs';
+    $(this).addClass(activeListClass).parents('.tabs-list').find('.' + ListItemClass).not($(this)).removeClass(activeListClass);
+    $(this).parents(tabWrap).children('.tab').removeClass('tab--active');
+    $(tabActive).addClass('tab--active');
+  });
+});
