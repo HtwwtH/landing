f3ce38b58e4987ff4b0cd33c23d2cfb0be861313
Alina Povazhnyuk <alina.l22@mail.ru>
2021-07-20 15:38:28 +0700

    format link fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,22 @@
-$(function(){
-    $('.tabs-list__link').click(function(e){
-      e.preventDefault();
+$(function () {
 
-      var tabActive = $(this).attr('href'),
-          activeListClass = 'tabs-list__link--active',
-          ListItemClass = 'tabs-list__link',
-          tabWrap = '.tabs'
+  $('a[data-rel^=lightcase]').lightcase();
 
-      $(this).addClass(activeListClass).parents('.tabs-list').find('.' + ListItemClass).not($(this)).removeClass(activeListClass);
-      $(this).parents(tabWrap).children('.tab').removeClass('tab--active');
-      $(tabActive).addClass('tab--active');
-    });
+  $('.select-styler').select2({
+    width: '100%',
+    minimumResultsForSearch: -1
+  });
 
-    $('a[data-rel^=lightcase]').lightcase();
+  $('.select-styler-filter').select2({
+    width: '100%',
+    minimumResultsForSearch: -1,
+    dropdownCssClass: "filter"
+  });
 
-    $('.select-styler').select2({
-        width: '100%',
-        minimumResultsForSearch: -1
-    });
-
-    $('.select-styler-filter').select2({
-      width: '100%',
-      minimumResultsForSearch: -1,
-      dropdownCssClass: "filter"
-    });
-
-    $('.group-title').click(function(){
-      $(this).toggleClass('group-title-collapsed');
-      $(`.${$(this).data('collapse')}`).toggle();
-    })
+  $('.group-title').click(function () {
+    $(this).toggleClass('group-title-collapsed');
+    $(`.${$(this).data('collapse')}`).toggle();
+  })
 });
 
 
