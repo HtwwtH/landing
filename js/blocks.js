70c36b8a9873706a9037491b5b8b86ecd29f0c2d
Alina Povazhnyuk <alina.l22@mail.ru>
2021-07-29 15:19:40 +0700

    анимация прозрачности блока с молнией, варианты главного и 2го блока вынесены в отдельные

--- js/blocks.js
+++ js/blocks.js
@@ -78,6 +78,20 @@ $(document).ready(function () {
 });
 "use strict";
 
+$(function () {
+  $('.tabs-list__link').click(function (e) {
+    e.preventDefault();
+    var tabActive = $(this).attr('href'),
+        activeListClass = 'tabs-list__link--active',
+        ListItemClass = 'tabs-list__link',
+        tabWrap = '.tabs';
+    $(this).addClass(activeListClass).parents('.tabs-list').find('.' + ListItemClass).not($(this)).removeClass(activeListClass);
+    $(this).parents(tabWrap).children('.tab').removeClass('tab--active');
+    $(tabActive).addClass('tab--active');
+  });
+});
+"use strict";
+
 $(document).ready(function () {
   $('.featured-catalog__trigger').on('click', function () {
     $('body').addClass('modal-open');
@@ -90,17 +104,53 @@ $(document).ready(function () {
 });
 "use strict";
 
-$(function () {
-  $('.tabs-list__link').click(function (e) {
-    e.preventDefault();
-    var tabActive = $(this).attr('href'),
-        activeListClass = 'tabs-list__link--active',
-        ListItemClass = 'tabs-list__link',
-        tabWrap = '.tabs';
-    $(this).addClass(activeListClass).parents('.tabs-list').find('.' + ListItemClass).not($(this)).removeClass(activeListClass);
-    $(this).parents(tabWrap).children('.tab').removeClass('tab--active');
-    $(tabActive).addClass('tab--active');
+function reliabilityAnimations2() {
+  var controller = new ScrollMagic.Controller();
+  var reliabilityFeatures = document.querySelectorAll('.reliability__fade');
+  reliabilityFeatures.forEach(function (feature, index, arr) {
+    var tl = new TimelineMax();
+    var previous = arr[0];
+    feature.style.opacity = 0.5;
+
+    if (index >= 1) {
+      previous = arr[index - 1];
+    }
+
+    new ScrollMagic.Scene({
+      triggerElement: feature,
+      triggerHook: 0.6,
+      duration: '60%'
+    }) // .addIndicators()
+    .on('enter', function () {
+      // console.log('enter');
+      tl.to(previous, 0.45, {
+        opacity: 0.5
+      });
+      tl.to(feature, 0.45, {
+        opacity: 1
+      });
+    }).on('leave', function () {
+      // console.log('leave');
+      tl.to(previous, 0.35, {
+        opacity: 1
+      });
+      tl.to(feature, 0.35, {
+        opacity: 0.5
+      });
+    }).addTo(controller);
   });
+}
+
+function initAnimations() {
+  var isMobile = $(window).width() < 1200;
+
+  if (!isMobile) {
+    reliabilityAnimations2();
+  }
+}
+
+$(document).ready(function () {
+  initAnimations();
 });
 "use strict";
 
