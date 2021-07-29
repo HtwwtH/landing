376a4e21d7b25d90b0e6a88c2ffc8793853dc6ed
Alina Povazhnyuk <alina.l22@mail.ru>
2021-07-29 15:19:40 +0700

    анимация прозрачности блока с молнией, варианты главного и 2го блока вынесены в отдельные

---
+++ src/blocks/reliability/reliability.js
@@ -0,0 +1,45 @@
+
+function reliabilityAnimations2() {
+  var controller = new ScrollMagic.Controller();
+  const reliabilityFeatures = document.querySelectorAll('.reliability__fade');
+
+  reliabilityFeatures.forEach((feature, index, arr) => {
+    var tl = new TimelineMax();
+    let previous = arr[0];
+    feature.style.opacity = 0.5;
+
+    if (index >= 1) {
+      previous = arr[index - 1];
+    }
+
+    new ScrollMagic.Scene({
+      triggerElement: feature,
+      triggerHook: 0.6,
+      duration: '60%'
+    })
+      // .addIndicators()
+      .on('enter', function () {
+        // console.log('enter');
+        tl.to(previous, 0.45, { opacity: 0.5 });
+        tl.to(feature, 0.45, { opacity: 1 });
+      })
+      .on('leave', function () {
+        // console.log('leave');
+        tl.to(previous, 0.35, { opacity: 1 });
+        tl.to(feature, 0.35, { opacity: 0.5 });
+      })
+      .addTo(controller);
+  });
+}
+
+function initAnimations() {
+  const isMobile = $(window).width() < 1200;
+
+  if (!isMobile) {
+    reliabilityAnimations2();
+  }
+}
+
+$(document).ready(function () {
+  initAnimations();
+});
