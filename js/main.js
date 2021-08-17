1dd10ca55dae17d3f24eab30026ce4bb476514ac
Alina Povazhnyuk <alina.l22@mail.ru>
2021-08-17 11:13:10 +0700

    исправлено свойство overflow, header прибивается на пк

--- js/main.js
+++ js/main.js
@@ -18,14 +18,29 @@ $(function () {
   dropdown.each(function () {
     $(this).mouseenter(function () {
       if ($(window).width() >= '1200') {
-        $(this).find('.dropdown__menu').slideDown();
+        $(this).find('.dropdown__menu').fadeIn(100);
       }
     });
     $(this).find('.dropdown-wrapper').mouseleave(function (e) {
-      if (e.relatedTarget.className.indexOf('dropdown__menu') == -1) $('.dropdown__menu').slideUp();
+      if (e.relatedTarget.className.indexOf('dropdown__menu') == -1) $('.dropdown__menu').fadeOut(100);
     });
     $('.dropdown__menu').mouseleave(function (e) {
-      if (e.relatedTarget.className.indexOf('dropdown-wrapper') == -1) $('.dropdown__menu').slideUp();
+      if (e.relatedTarget.className.indexOf('dropdown-wrapper') == -1) $('.dropdown__menu').fadeOut(100);
     });
-  });
+  }); // fix header on scroll on pc
+
+  if ($(document).width() >= 1200) {
+    var lastScrollTop = 0;
+    var header = $('.header__main');
+    var headerOffset = header.offset().top;
+    $(window).scroll(function (event) {
+      var st = $(this).scrollTop();
+
+      if (st >= headerOffset) {
+        header.addClass('fixed');
+      } else if (header.hasClass('fixed') == true) header.removeClass('fixed');
+
+      lastScrollTop = st;
+    });
+  }
 });
